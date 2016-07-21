7f7d2c603ba348c57ab215ed916377576644e303
Maxis Kao <maxiskao@yahoo-inc.com>
2016-07-21 21:29:26 +0800

    init slackbot

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var Botkit = require('Botkit');
+// var Bot = require('slackbots');
+
+// token=xoxb-61820345316-WHkHw3f9ZPAYy2vVf4z6adx6 node index.js
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+// var settings = {
+//     token: process.env.token,
+//     name: 'BeautyBot'
+// };
+
+// var bot = new Bot(settings);
+
+// bot.on('start', function() {
+//     bot.postMessageToChannel('random', 'Hello channel!');
+//     // bot.postMessageToUser('some-username', 'hello bro!');
+//     // bot.postMessageToGroup('some-private-group', 'hello group chat!');
+// });
+
+var controller = Botkit.slackbot({ debug: false });
+
+var bot = controller.spawn({
+  token: process.env.token
+});
+
+bot.startRTM(function(err) {
+    if (err) {
+        throw new Error(err);
+    }
+});
+
+// controller.on('message_received', function(bot, message) {
+//     bot.reply(message, 'I heard... something!');
+// });
+
+controller.hears(['keyword'],['message_received'],function(bot,message) {
+
+  // do something to respond to message
+  bot.reply(message,'You used a keyword!');
+
+});
+
+
+// controller.spawn({
+//     token: process.env.token
+// });
+
+// controller.on('hello', function(bot,message) {
+
+//     // bot.
+//     console.log('>>> bot:', bot);
+//     console.log('>>> message:', message);
+
+//     // if (message.type === 'user_typing') {
+//     //     // do nothing
+//     // } else {
+//         // console.log('>>> message:', message);
+//     bot.reply(message, "Hello");
+//     // }
+// });
+
+// controller.hears(['(.*)'], ['direct_message','direct_mention','mention', 'message_received'], function(bot,message) {
+//     console.log('>>> message:', message);
+//     bot.reply(message, 'You saied '+message.text);
+// });
